41fdac7ba13e0fc36fca1585cf2d16e3ab8298cb
8002117062 <939945825@qq.com>
2019-10-07 22:00:55 -0700

    test8

--- test8/main.c
+++ test8/main.c
@@ -1,31 +1,48 @@
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include<stdlib.h>
+#include<unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
+#include<sys/stat.h>
+#include<string.h>
+ 
 
-int main()
+int main(void)
+
 {
-	int fd;
-	char buf1[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *buf = "helloWorld";
-	int flags =O_CREAT | O_TRUNC | O_WRONLY;
-	char buf2[5];
 
-	if((fd = open("test.dat",flags,0644)) < 0)
-		perror("opoen file error.");
-	if(write(fd,buf1,sizeof(buf1))!=sizeof(buf1))
-		perror("buf1 write into fd error!");
-	if(lseek(fd,5,SEEK_SET) < 0)
-		perror("lseek error!");
-	if(read(fd,buf2,(sizeof(buf2))) != (sizeof(buf2)))
-		//perror("read error!");
-	if(lseek(fd,10,SEEK_SET)<0)
-		perror("lseek error!");
-	if(write(fd,buf,strlen(buf)) != strlen(buf))
-		perror("write error!");
+ 
 
-	return 0;
+        int fd;
+
+        char buf1[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+ 		 char *buf ="helloworld";
+		char buf2[5];
+       
+
+        if ((fd = open("test2.txt",O_RDWR|O_CREAT,0644)) < 0)
+
+                printf("open error! \n");
+		if(write(fd,buf1,sizeof(buf1))!=sizeof(buf1))
+		perror("buf1 write into fd1 error");
+
+
+        if (lseek(fd,5,SEEK_SET) < 0) 
+ 
+                printf("lseek error! \n");
+
+        if  (read(fd,buf2,(sizeof(buf2))) != (sizeof(buf2)))   
+
+                printf("read error! \n");
+
+        else
+				buf2[5] = '\0';
+                printf("read:%s\n",buf2);
+	if (lseek(fd,10,SEEK_SET) < 0) 
+ 
+                printf("lseek error! \n");	
+       if (write(fd,buf,strlen(buf)) != strlen(buf))    
+
+              printf("write error! \n");
+
 }
