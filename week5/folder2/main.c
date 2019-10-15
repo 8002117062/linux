97e0e272b23377a0d877fd4f2178e79b011d1ea6
8002117062 <939945825@qq.com>
2019-10-15 01:42:55 -0700

    week5 homework commit

---
+++ week5/folder2/main.c
@@ -0,0 +1,15 @@
+#include "/home/rlk/week5/folder1/maxmain.h"
+
+int main(void)
+{
+	int data[100];
+	int i,result;
+	getdata(data,100);
+	for(i = 0;i < 100;i++)
+	{
+		printf("data[%d]:%d\n",i, data[i]);
+	}
+	result = getmax(data,100);
+	printf("the max of the array is:%d\n",result);
+	return 0;
+}
