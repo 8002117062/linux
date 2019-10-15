d8b46c94295968c0a78306fc3bedbb52ec8ccf52
8002117062 <939945825@qq.com>
2019-10-15 01:30:02 -0700

    delete all

--- test3/funhead.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-#include <unistd.h>
-
-long int get_qsort_time(int a[]);
-long int get_my_time(int b[]);
-int compar(const void *a,const void *b);
